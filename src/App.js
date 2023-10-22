5edc7f8b53cbc4d22a2396363cf81075237b176b
Ivan <ivanstoman100@gmail.com>
2023-10-22 17:37:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
+import axios from "axios";
+import ItemDetail from "./pages/ItemDetail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const [posts,setPosts] = useState([])
+
+        const fetchData = async () => {
+            const res = await axios.get('https://hacker-news.firebaseio.com/v0/newstories.json')
+            setPosts(res.data.slice(0, 100))
+        }
+
+    useEffect(() => {
+
+        fetchData()
+
+        const intervalId = setInterval(() => {
+            fetchData();
+        }, 60000);
+
+        return () => clearInterval(intervalId);
+    }, [])
+
+    return (
+        <div className='main'>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Main posts={posts} fetchData={fetchData}/>}/>
+                    <Route path='/:id' element={<ItemDetail/>}/>
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
